dd48c4a28f52843dacc7af858da789617c250218
piggypiggy <1091267149@qq.com>
2021-01-09 16:08:18 +0800

    ios

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-    printf("...");
+    unsigned int num=0x7fd00;
+    unsigned int i;
+
+    for(i=256;i<512;i++){
+        if(i%8==0)
+        printf("\n");
+        printf("0x%x, ", num/i);
+    }
     return 0;
 }
